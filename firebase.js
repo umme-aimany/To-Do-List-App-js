e34db1a777b9f83c22ac3acaf90dcd884126e676
Umme Aiman <122363026+umme-aimany@users.noreply.github.com>
2023-08-24 19:25:57 +0500

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyC-mD2PMfvsaa5BC4iCs5v2GvynfI64HJI",
+    authDomain: "to--do-list-app-43f5c.firebaseapp.com",
+    projectId: "to--do-list-app-43f5c",
+    storageBucket: "to--do-list-app-43f5c.appspot.com",
+    messagingSenderId: "816019078215",
+    appId: "1:816019078215:web:12ffdef14dbc258b764562",
+    measurementId: "G-GVN86WK2H1"
+  };
